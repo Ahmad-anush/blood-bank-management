7de34a267c5ab279081645e09869c1526afcbfed
Ahmad Anosh <82706072+Ahmad-anush@users.noreply.github.com>
2022-02-08 17:23:41 +0530

    Add files via upload

---
+++ BDManagement/layout/_footer.php
@@ -0,0 +1,10 @@
+<footer class="foter">
+            <b>Made By: Asadullah Payenda (17001378) Student Of BCA Batch-17 </a></b>
+            <br>
+            &COPY;AP Goyal Shimla University
+</footer>
+
+
+
+</body>
+</html>
---
+++ BDManagement/users/layout/_footer.php
@@ -0,0 +1,15 @@
+<footer class="foter">
+            <b>Made By: Asadullah Payenda (17001378) Student Of BCA Batch-17 </a></b>
+            <br>
+            &COPY;AP Goyal Shimla University
+</footer>
+
+
+
+</body>
+</html>
+
+
+
+</body>
+</html>
